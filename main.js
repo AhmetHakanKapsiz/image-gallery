c557b970639f3d9f50c058efd8f6dd98c41f6025
AhmetHakanKapsiz <ahmethakankapsiz@gmail.com>
2022-03-10 18:07:07 +0300

    added basic code to main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const uploadContainer = document.querySelector('#upload-container');
+const imageContainer = document.querySelector('#image-container');
+const input = document.querySelector('#upload');
+const canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+
+const fileTypes = [
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "image/tiff",
+    "image/webp",
+    "image/jpg",
+];
+
+function validFileType(file) {
+    return fileTypes.includes(file.type);
+}
+
+input.addEventListener('change', e => {
+    const file = input.files[0];
+    console.log(file)
+    if (validFileType(file)) {
+        let img = document.createElement('img');
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0, 400, 400);
+        }
+        img.src = URL.createObjectURL(file);
+    }
+
+});
