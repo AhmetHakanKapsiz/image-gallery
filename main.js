70e87be9ef9c117acb563e9aa911717fd6e3a8f2
AhmetHakanKapsiz <ahmethakankapsiz@gmail.com>
2022-03-15 17:56:01 +0300

    add some image processing and image downloading

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const uploadDiv = document.querySelector('#upload-div');
 const canvasDiv = document.querySelector('#canvas-div');
 const input = document.querySelector('#upload');
 const canvas = document.querySelector('#canvas');
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 const fileTypes = [
     "image/gif",
@@ -49,16 +49,48 @@ input.addEventListener('change', e => {
         }
         img.src = URL.createObjectURL(file);
 
-        const btn = document.createElement('button');
-        btn.textContent = "Save it to the gallery";
-        canvasDiv.appendChild(btn);
+        const btnSave = document.createElement('button');
+        btnSave.textContent = "Save it to the gallery";
+        canvasDiv.appendChild(btnSave);
 
-        btn.addEventListener('click', ev => {
-            btn.remove();
-            let images = db.transaction('images', 'readwrite').objectStore('images');
-            let req = images.add({ image: file });
+        btnSave.addEventListener('click', ev => {
+            btnSave.remove();
+            btnInvert.remove();
+            btnContrastRed.remove();
+            btnContrastInc.remove();
 
-            req.onsuccess = () => display();
+            canvas.toBlob(blob => {
+                let images = db.transaction('images', 'readwrite').objectStore('images');
+                let req = images.add({ image: blob });
+
+                req.onsuccess = () => display();
+            }, 'image/png', 1);
+            ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);
+            
+        });
+
+        const btnInvert = document.createElement('button');
+        btnInvert.textContent = 'Invert';
+        canvasDiv.appendChild(btnInvert);
+
+        btnInvert.addEventListener('click', () => {
+            invert();
+        });
+
+        const btnContrastRed = document.createElement('button');
+        btnContrastRed.textContent = 'reduce contrast';
+        canvasDiv.appendChild(btnContrastRed);
+
+        btnContrastRed.addEventListener('click', () => {
+            contrastReduce();
+        });
+
+        const btnContrastInc = document.createElement('button');
+        btnContrastInc.textContent = 'Increase contrast';
+        canvasDiv.appendChild(btnContrastInc);
+
+        btnContrastInc.addEventListener('click', () => {
+            contrastIncrease();
         });
     }
 
@@ -73,19 +105,22 @@ function display() {
         for (const item of images) {
             let div = document.createElement('div');
             div.classList.add('image-card');
-/*
-            let p = document.createElement('p');
-            p.textContent = item.name;*/
 
+            let url = URL.createObjectURL(item.image);
             let img = new Image(250, 250);
-            img.src = URL.createObjectURL(item.image);
+            img.src = url;
 
             let btn = document.createElement('button');
             btn.textContent = 'Remove';
 
+            let a = document.createElement('a');
+            a.href = url;
+            a.textContent = 'Download';
+            a.download = "Image.png";
+
             div.appendChild(img);
-            //div.appendChild(p);
             div.appendChild(btn);
+            div.appendChild(a);
 
             imageContainer.appendChild(div);
 
@@ -97,4 +132,56 @@ function display() {
             });
         }
     };
+}
+
+function invert() {
+    const image = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+    const data = image.data;
+
+    for (let index = 0; index < data.length; index += 4) {
+        data[index] = 255 - data[index];
+        data[index + 1] = 255 - data[index + 1];
+        data[index + 2] = 255 - data[index + 2];
+
+    }
+    ctx.putImageData(image, 0, 0);
+
+}
+
+function contrastIncrease() {
+    const image = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+    const data = image.data;
+
+    for (let index = 0; index < data.length; index += 4) {
+        if (data[index] + 10 < 255) {
+            data[index] += 10;
+        }
+        if (data[index + 1] + 10 < 255) {
+            data[index + 1] += 10;
+        }
+        if (data[index + 2] < 255) {
+            data[index + 2] += 10;
+        }
+    }
+
+    ctx.putImageData(image, 0, 0);
+}
+
+function contrastReduce() {
+    const image = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+    const data = image.data;
+    console.log(data);
+    for (let index = 0; index < data.length; index += 4) {
+        if (data[index] - 10 > 0) {
+            data[index] -= 10;
+        }
+        if (data[index + 1] - 10 > 0) {
+            data[index + 1] -= 10;
+        }
+        if (data[index + 2] - 10 > 0) {
+            data[index + 2] -= 10;
+        }
+    }
+
+    ctx.putImageData(image, 0, 0);
 }
